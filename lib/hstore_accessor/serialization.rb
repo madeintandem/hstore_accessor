47341471443b2ee4fde6b41b8b491327b901b0c7
Tony Coconate <me@tonycoconate.com>
2014-08-20 15:59:11 -0500

    allow nils and formatting

--- lib/hstore_accessor/serialization.rb
+++ lib/hstore_accessor/serialization.rb
@@ -9,6 +9,8 @@ module HstoreAccessor
 
     SERIALIZERS = {
       array: -> value { (value && YAML.dump(Array.wrap(value))) || nil },
+      boolean: -> value { (value.to_s == "true").to_s },
+      date: -> value { (value && value.to_s) || nil },
       hash: lambda do |value|
         if value
           raise InvalidDataTypeError, "Cannot serialize a non-hash value into the hash typed attribute" unless value.is_a?(Hash)
@@ -17,20 +19,18 @@ module HstoreAccessor
           nil
         end
       end,
-      time: -> value { value.to_i },
-      boolean: -> value { (value.to_s == "true").to_s },
-      date: -> value { (value && value.to_s) || nil }
+      time: -> value { (value && value.to_i) || nil }
     }
 
     DESERIALIZERS = {
       array: -> value { (value && YAML.load(value)) || nil },
-      hash: -> value { (value && YAML.load(value)) || nil },
-      integer: -> value { value.to_i },
-      float: -> value { value.to_f },
-      time: -> value { Time.at(value.to_i) },
       boolean: -> value { TypeHelpers.cast(:boolean, value) },
       date: -> value { (value && Date.parse(value)) || nil },
-      decimal: -> value { BigDecimal.new(value) }
+      decimal: -> value { (value && BigDecimal.new(value)) || nil },
+      float: -> value { (value && value.to_f) || nil },
+      hash: -> value { (value && YAML.load(value)) || nil },
+      integer: -> value { (value && value.to_i) || nil },
+      time: -> value { (value && Time.at(value.to_i)) || nil }
     }
 
     def serialize(type, value, serializer=nil)
