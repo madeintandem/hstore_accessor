6abc906a74b2becffe07b551043567c8d3f59946
Michael Crismali <michael.crismali@gmail.com>
2014-12-30 21:28:39 -0600

    Made TypeHelpers use ActiveRecord type casting for floats

--- lib/hstore_accessor/type_helpers.rb
+++ lib/hstore_accessor/type_helpers.rb
@@ -9,7 +9,7 @@ module HstoreAccessor
       when :integer
         ActiveRecord::Type::Integer.new.type_cast_from_user(value)
       when :float
-        value.to_f
+        ActiveRecord::Type::Float.new.type_cast_from_user(value)
       when :time
         ActiveRecord::Type::DateTime.new.type_cast_from_user(value)
       when :date
