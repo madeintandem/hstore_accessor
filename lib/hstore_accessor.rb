9924eb0c790b5f4b5ffd2772e78ff3395d38b53b
Derek Kniffin <derek.kniffin@colorado.edu>
2015-02-19 14:35:26 -0700

    Removing less-than symbol from directory name

--- lib/hstore_accessor.rb
+++ lib/hstore_accessor.rb
@@ -5,8 +5,8 @@ require "hstore_accessor/version"
 if ::ActiveRecord::VERSION::STRING.to_f >= 4.2
   require "hstore_accessor/active_record_4.2/type_helpers"
 else
-  require "hstore_accessor/active_record_<_4.2/type_helpers"
-  require "hstore_accessor/active_record_<_4.2/time_helper"
+  require "hstore_accessor/active_record_pre_4.2/type_helpers"
+  require "hstore_accessor/active_record_pre_4.2/time_helper"
 end
 
 require "hstore_accessor/serialization"
