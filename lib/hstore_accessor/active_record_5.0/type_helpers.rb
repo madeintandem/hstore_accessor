2cfb963154f2e29227dc8ca00e5b0000b8b62be3
Hansel Wave <arastafiel@gmail.com>
2016-07-07 15:47:01 -0600

    Add some support for rails 5

---
+++ lib/hstore_accessor/active_record_5.0/type_helpers.rb
@@ -0,0 +1,34 @@
+module HstoreAccessor
+  module TypeHelpers
+    TYPES = {
+      boolean: ActiveRecord::Type::Boolean,
+      date: ActiveRecord::Type::Date,
+      datetime: ActiveRecord::Type::DateTime,
+      decimal: ActiveRecord::Type::Decimal,
+      float: ActiveRecord::Type::Float,
+      integer: ActiveRecord::Type::Integer,
+      string: ActiveRecord::Type::String
+    }
+
+    TYPES.default = ActiveRecord::Type::Value
+
+    class << self
+      def column_type_for(attribute, data_type)
+        ActiveRecord::ConnectionAdapters::Column.new(attribute.to_s, nil, TYPES[data_type].new)
+      end
+
+      def cast(type, value)
+        return nil if value.nil?
+
+        case type
+        when :string, :decimal
+          value
+        when :integer, :float, :datetime, :date, :boolean
+          TYPES[type].new.cast(value)
+        else value
+          # Nothing.
+        end
+      end
+    end
+  end
+end
